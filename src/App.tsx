12ed105171398901e0edb479cf94985716ba586b
tothricsaj <tothricsaj@gmail.com>
2020-10-05 13:39:23 +0200

    MOvieitem is added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 import Header from './components/Header/Header'
+import Moviecontainer from './components/Moviecontainer/Moviecontainer';
 
 function App() {
   return (
     <ErrorBoundary>
       <div className="App">
           <Header />
+          <Moviecontainer />
       </div>
     </ErrorBoundary>
   );
---
+++ src/components/Moviecontainer/Moviecontainer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+interface Props {
+
+}
+
+const Moviecontainer: React.FC<Props> = (props) => {
+    return (
+        <div className="moviecontainer">
+            Moviecontainer
+        </div>
+    )
+}
+
+export default Moviecontainer
